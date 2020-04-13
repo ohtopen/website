ec3748d6237dc85c8ed5b711edf62a41fbb4e273
ohtopen <47524838+ohtopen@users.noreply.github.com>
2020-04-13 15:52:34 +0300

    fixing bugs

--- src/components/resepti.js
+++ src/components/resepti.js
@@ -6,9 +6,9 @@ class Resepti extends Component {
     render() {
         return ( 
             <div className="resepti-body">
-            <h4> (lähes) VEGAANI SITRUUNAPASTARESEPTI(4 hengelle) </h4>
+            <h4> (lähes) VEGAANI SITRUUNAPASTA (4 hengelle) </h4>
 
-            <p>Paketti härkäpapusuikaleita(esim Beanit) </p>
+            <p>Paketti härkäpapusuikaleita (esim Beanit) </p>
             <p>loraus öljyä </p>
             <p>1 pieni sipuli pilkottuna </p>
             <p>ripaus cayennepippuria </p>
@@ -19,7 +19,7 @@ class Resepti extends Component {
             <p>1-2 tl raastettua inkivääriä </p>
             <p>2 rkl sitruunan mehua </p>
             <p>2 rkl hunajaa </p>
-            <p>2 dl maustamatonta kaurajogurttia(mieluiten turkkilaista) </p>
+            <p>2 dl maustamatonta kaurajogurttia (mieluiten turkkilaista) </p>
             <p>suolaa </p>
             <p>noin 200 g spagettia </p>
             <p>tuoreita yrttejä</p>
@@ -30,7 +30,7 @@ class Resepti extends Component {
 
             <p>Pilko sipuli, paista miedolla lämmöllä kunnes kuullotettu.</p>
             <p>Lisää suikaleet sekä cayennepippuri.</p>
-            <p>Sekoita joukkoon kerma ja fondi. Anna kastikkeen kypsyä hetki.(tässä välissä voit laittaa spagettiveden kiehumaan)</p>
+            <p>Sekoita joukkoon kerma ja fondi. Anna kastikkeen kypsyä hetki. (tässä välissä voit laittaa spagettiveden kiehumaan)</p>
             <p>Lisää kurkuma, valkosipuli, inkivääri, sitruunan mehu ja hunaja. Jatka kypsennystä hiljaisella tulella.</p>
             <p>Lisää lopuksi kaurajogurtti ja suolaa maun mukaan.</p>
             <p>Sekoita kastike hyvin valutetun pastan joukkoon. Tarjoa ruoka syviltä lautasilta ja ripottele päälle raastettua juustoa.</p>
